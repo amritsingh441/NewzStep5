5a6ab8f1733d0e669a36b4bb7ca589ed0582d8a5
durai <d.duraivelan@stackroute.in>
2020-02-27 20:13:05 +0530

    NewzApp Step5

---
+++ NewsService/src/main/java/com/stackroute/newz/model/NewsSource.java
@@ -0,0 +1,58 @@
+package com.stackroute.newz.model;
+
+import java.util.Date;
+
+public class NewsSource {
+	
+	/*
+	 * This class should have five fields
+	 * (newssourceId,newssourceName,newssourceDesc,newssourceCreatedBy,newssourceCreationDate).
+	 * This class should also contain the getters and setters for the 
+	 * fields along with the parameterized	constructor and toString method.
+	 * The value of newssourceCreationDate should not be accepted from the user but should be
+	 * always initialized with the system date.
+	 */
+	
+
+	public String getNewsSourceId() {
+		return null;
+	}
+
+	public void setNewsSourceId(int newssourceId) {
+
+	}
+
+	public String getNewsSourceName() {
+		return null;
+	}
+
+	public void setNewsSourceName(String newssourceName) {
+	
+	}
+
+	public String getNewsSourceDesc() {
+		return null;
+	}
+
+	public void setNewsSourceDesc(String newssourceDesc) {
+		
+	}
+
+	public String getNewsSourceCreatedBy() {
+		return null;
+	}
+
+	public void setNewsSourceCreatedBy(String newssourceCreatedBy) {
+		
+	}
+
+	public Date getNewsSourceCreationDate() {
+		return null;
+	}
+
+	public void setNewsSourceCreationDate() {
+
+	}
+
+
+}
