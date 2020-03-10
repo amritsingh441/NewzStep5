86f8e0ad43457d4f8edf2c3271de1427abf2937c
ubuntu <ubuntu@ip-172-31-32-139.ec2.internal>
2020-03-10 12:40:12 +0000

    initial commit

--- UserProfileService/src/main/java/com/stackroute/userprofile/jwtfilter/JwtFilter.java
+++ UserProfileService/src/main/java/com/stackroute/userprofile/jwtfilter/JwtFilter.java
@@ -1,22 +1,30 @@
 package com.stackroute.userprofile.jwtfilter;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.filter.GenericFilterBean;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
 
 /* This class implements the custom filter by extending org.springframework.web.filter.GenericFilterBean.  
  * Override the doFilter method with ServletRequest, ServletResponse and FilterChain.
  * This is used to authorize the API access for the application.
  */
 
-public class JwtFilter extends GenericFilterBean {
+public class JwtFilter extends GenericFilterBean{
 
-		/*
+	/*
 	 * Override the doFilter method of GenericFilterBean.
      * Retrieve the "authorization" header from the HttpServletRequest object.
      * Retrieve the "Bearer" token from "authorization" header.
@@ -24,13 +32,64 @@ public class JwtFilter extends GenericFilterBean {
      * Parse the JWT token and get claims from the token using the secret key
      * Set the request attribute with the retrieved claims
      * Call FilterChain object's doFilter() method */
-	
-	
-    @Override
+
+	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
+		HttpServletRequest req=(HttpServletRequest)request;
+		HttpServletResponse res=(HttpServletResponse)response;
+		if(req.getMethod().equalsIgnoreCase(HttpMethod.OPTIONS.name()))
+		{
+			chain.doFilter(req, res);
+		}
+		
+		else
+		{
+			String authen=req.getHeader("Authorization");
+			
+			res.setHeader("Access-Control-Allow-Origin",req.getHeader("origin"));
+			res.setHeader("Access-Control-Allow-Credentials", "true");
+			res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTION,DELETE,PATCH");
+			
+			
+			if((authen==null)|| !authen.startsWith("Bearer"))
+			{
+				throw new ServletException("not found");
+			}
+			
+			final String compact=authen.substring(7);
+			
+			try
+			{
+				JwtParser parseobj=Jwts.parser().setSigningKey("secretKey");
+				
+				Jwt jwt=parseobj.parse(compact);
+				
+				Claims claim=(Claims)jwt.getBody();
+				
+				req.setAttribute("claims", claim)	;
+				
+				String sub=claim.getSubject();
+				HttpSession sess=req.getSession();
+				sess.setAttribute("login", sub);
+				System.out.println("user" + sub);
+				System.out.println("Compact value" + compact);
+				
+				
+			}
+			catch(SignatureException e)
+			{
+			throw new ServletException("invaid key");	
+			}
+			catch(MalformedJwtException e)
+			{
+				throw new ServletException("key modified by unauthorized person");
+				
+			}
+			
+					chain.doFilter(req, res);
+		}
 	}
 
 }
-
--- UserProfileService/src/main/java/com/stackroute/userprofile/service/UserProfileServiceImpl.java
+++ UserProfileService/src/main/java/com/stackroute/userprofile/service/UserProfileServiceImpl.java
@@ -1,19 +1,26 @@
 package com.stackroute.userprofile.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import com.stackroute.userprofile.model.UserProfile;
+import com.stackroute.userprofile.repository.UserProfileRepository;
 import com.stackroute.userprofile.util.exception.UserProfileAlreadyExistsException;
 import com.stackroute.userprofile.util.exception.UserProfileNotFoundException;
 
 /*
-* Service classes are used here to implement additional business logic/validation 
-* This class has to be annotated with @Service annotation.
-* @Service - It is a specialization of the component annotation. It doesn't currently 
-* provide any additional behavior over the @Component annotation, but it's a good idea 
-* to use @Service over @Component in service-layer classes because it specifies intent 
-* better. Additionally, tool support and additional behavior might rely on it in the 
-* future.
-* */
+ * Service classes are used here to implement additional business logic/validation 
+ * This class has to be annotated with @Service annotation.
+ * @Service - It is a specialization of the component annotation. It doesn't currently 
+ * provide any additional behavior over the @Component annotation, but it's a good idea 
+ * to use @Service over @Component in service-layer classes because it specifies intent 
+ * better. Additionally, tool support and additional behavior might rely on it in the 
+ * future.
+ * */
 
+@Service
+@Component
 public class UserProfileServiceImpl implements UserProfileService {
 
 	/*
@@ -21,43 +28,49 @@ public class UserProfileServiceImpl implements UserProfileService {
 	 * Constructor-based autowiring) Please note that we should not create any
 	 * object using the new keyword.
 	 */
-
+	private UserProfileRepository userProfileRepository;
+	
+	@Autowired
+	public UserProfileServiceImpl (UserProfileRepository userProfileRepository) {
+		this.userProfileRepository = userProfileRepository;
+	}
 	/*
 	 * This method should be used to save a new userprofile.Call the corresponding method
 	 * of Respository interface.
 	 */
 
-    public UserProfile registerUser(UserProfile user) throws UserProfileAlreadyExistsException {
-        return null;
-    }
+	public UserProfile registerUser(UserProfile user) throws UserProfileAlreadyExistsException {
+		//userProfileRepository.insert(user);
+		return null;
+	}
 
 	/*
 	 * This method should be used to update a existing userprofile.Call the corresponding
 	 * method of Respository interface.
 	 */
 
-    @Override
-    public UserProfile updateUser(String userId, UserProfile user) throws UserProfileNotFoundException {
-    	return null;
-    }
+	@Override
+	public UserProfile updateUser(String userId, UserProfile user) throws UserProfileNotFoundException {
+		return null;
+	}
 
 	/*
 	 * This method should be used to delete an existing user. Call the corresponding
 	 * method of Respository interface.
 	 */
 
-    @Override
-    public boolean deleteUser(String userId) throws UserProfileNotFoundException {
-        return false;
-    }
-    
+	@Override
+	public boolean deleteUser(String userId) throws UserProfileNotFoundException {
+		return false;
+	}
+
 	/*
 	 * This method should be used to get userprofile by userId.Call the corresponding
 	 * method of Respository interface.
 	 */
 
-    @Override
-    public UserProfile getUserById(String userId) throws UserProfileNotFoundException {
-    	return null;
-    }
+	@Override
+	public UserProfile getUserById(String userId) throws UserProfileNotFoundException {
+		return null;
+	}
 }
